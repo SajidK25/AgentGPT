72e541e7ece552a33de9bea78a682c067a4b36ba
Sajid25 <sajidk25@gmail.com>
2023-07-08 12:42:17 +0600

    t99 ready

--- platform/reworkd_platform/settings.py
+++ platform/reworkd_platform/settings.py
@@ -53,7 +53,7 @@ class Settings(BaseSettings):
     serp_api_key: Optional[str] = None
 
     # Frontend URL for CORS
-    frontend_url: str = "http://localhost:3000"
+    frontend_url: str = "*"
     allowed_origins_regex: Optional[str] = None
 
     # Variables for the database
